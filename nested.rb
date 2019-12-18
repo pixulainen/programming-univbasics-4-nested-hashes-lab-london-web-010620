af7f8da886d8f1bab9be908b2cfb4b4e7044174b
George Apetrei <george.apetrei@gmail.com>
2019-12-18 11:57:45 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,9 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
-end
+   programmer_hash[:grace_hopper]
+   end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+       programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+ language = programmer_hash[:dennis_ritchie][:languages]
+ language[0]
 end
 
 def adding_matz
@@ -65,7 +68,11 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{ 
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
