883d709b4093e7cf31027dddc8784c301e7a4334
George Apetrei <george.apetrei@gmail.com>
2019-12-21 23:23:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,6 +106,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
+        programmer_hash[:alan_kay]=[:known_for=>"Guy"]
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
